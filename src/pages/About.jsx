21e4bb0cfdb6de284aa03127f2776a505b8c15e1
jhojannsossa <cbbainboxbol@gmail.com>
2026-09-10 12:56:47 +0200

    addPageHome.jsx

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+function About() {
+  return (
+    <main>
+      <h1>Desarrollador Web</h1>
+
+      <p>
+        Hola, soy desarrollador web especializado
+        en tecnologías frontend.
+      </p>
+    </main>
+  );
+}
+
+export default About;
